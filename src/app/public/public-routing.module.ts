394ff3e2cae84d551d5e1e6d42672c8d6083a0c6
christian <chrisxdd499@gmail.com>
2024-06-05 15:36:16 -0500

    se implementa correccion del layout

--- src/app/public/public-routing.module.ts
+++ src/app/public/public-routing.module.ts
@@ -1,13 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LayoutMainComponent } from '../ui/layouts/layout-main/layout-main.component';
+import { AdminManagmentContainerComponent } from '../containers/admin-managment-container/admin-managment-container.component';
+import { RequestProviderContainerComponent } from '../containers/request-provider-container/request-provider-container.component';
 
 const routes: Routes = [
-{
-  path:'',
-  component: LayoutMainComponent
-}
 
+{
+  path:'admin-managment',
+  component: LayoutMainComponent,
+  children:[
+    {
+      path:'',
+       component: AdminManagmentContainerComponent, 
+       outlet: 'main'
+    }
+  ],
+},
+{
+  path:'request-providers',
+  component: LayoutMainComponent,
+  children:[
+    {
+      path:'',
+       component: RequestProviderContainerComponent, 
+       outlet: 'main'
+    }
+  ],
+}
 ];
 
 @NgModule({
@@ -15,3 +35,6 @@ const routes: Routes = [
   exports: [RouterModule],
 })
 export class PublicRoutingModule {}
+
+
+
