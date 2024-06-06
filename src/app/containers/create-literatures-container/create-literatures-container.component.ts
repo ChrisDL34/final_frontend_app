c4bc1b5601e0a931a9f55b7558dcc5cda6ac6de8
christian <chrisxdd499@gmail.com>
2024-06-06 17:11:20 -0500

    se realiza implementacion de la creacion de literatures-component

---
+++ src/app/containers/create-literatures-container/create-literatures-container.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CreateLiteraturesComponent } from '../../ui/forms/create-literatures/create-literatures.component';
+
+@Component({
+  selector: 'app-create-literatures-container',
+  standalone: true,
+  imports: [CreateLiteraturesComponent],
+  templateUrl: './create-literatures-container.component.html',
+
+})
+export class CreateLiteraturesContainerComponent {
+
+}
