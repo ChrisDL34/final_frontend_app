ca0e2747bd5c4e152824e566f9b955fb8695d702
christian <chrisxdd499@gmail.com>
2024-06-06 12:45:45 -0500

    se realiza implementacion de estilos para admin managment component

--- src/app/ui/blocks/admin-managment/admin-managment.component.ts
+++ src/app/ui/blocks/admin-managment/admin-managment.component.ts
@@ -1,12 +1,45 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-admin-managment',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './admin-managment.component.html',
-  styleUrl: './admin-managment.component.css'
+  styleUrls: ['./admin-managment.component.css']
 })
 export class AdminManagmentComponent {
+  constructor(private router: Router) {}
 
+  usuarios = [
+    { username: 'john_doe', email: 'john@example.com', rol: 'admin' },
+    { username: 'jane_smith', email: 'jane@example.com', rol: 'user' },
+    { username: 'jane_smith', email: 'jane@example.com', rol: 'provider' },
+    { username: 'jane_smith', email: 'jane@example.com', rol: 'provider' },
+    { username: 'jane_smith', email: 'jane@example.com', rol: 'provider' },
+    { username: 'jane_smith', email: 'jane@example.com', rol: 'provider' },
+    { username: 'jane_smith', email: 'jane@example.com', rol: 'provider' },
+    { username: 'jane_smith', email: 'jane@example.com', rol: 'provider' },
+    { username: 'jane_smith', email: 'jane@example.com', rol: 'provider' },
+    { username: 'jane_smith', email: 'jane@example.com', rol: 'provider' },
+    { username: 'jane_smith', email: 'jane@example.com', rol: 'provider' }
+  ];
+
+  editarUsuario(usuario: any) {
+    
+  
+  }
+
+  eliminarUsuario(usuario: any) {
+    
+  }
+
+  pedidoUsuario(usuario: any) {
+    this.router.navigate(['/request-providers']);
+  }
+
+  agregarUsuario() {
+    this.router.navigate(['/request-providers']);
+  }
 }
