9b7d03e7894c4cec241820d0a201b088ba2c160c
Daniel Rmirez Luna <danielramire1996@gmail.com>
2024-06-07 15:18:11 -0500

    ng delete

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,9 +3,8 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
-import { provideStoreDevtools } from '@ngrx/store-devtools';
 
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideHttpClient(), provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() })]
+  providers: [provideRouter(routes), provideHttpClient(),]
 };
